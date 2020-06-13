01d0917dd089ae381384496f98438c41b9d8bfe5
unknown <archerzou>
2020-06-14 00:08:19 +0800

    initial commit

---
+++ src/components/Recipe.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { addShipping,subShipping} from './actions/cartActions'
+class Recipe extends Component{
+    
+    componentWillUnmount() {
+         if(this.refs.shipping.checked)
+              this.props.subShipping()
+    }
+
+    handleChecked = (e)=>{
+        if(e.target.checked){
+            this.props.addShipping();
+        }
+        else{
+            this.props.subShipping();
+        }
+    }
+
+    render(){
+  
+        return(
+            <div className="container">
+                <div className="collection">
+                    <li className="collection-item">
+                            <label>
+                                <input type="checkbox" ref="shipping" onChange= {this.handleChecked} />
+                                <span>Shipping(+6$)</span>
+                            </label>
+                        </li>
+                        <li className="collection-item"><b>Total: {this.props.total} $</b></li>
+                    </div>
+                    <div className="checkout">
+                        <button className="waves-effect waves-light btn">Checkout</button>
+                    </div>
+                 </div>
+        )
+    }
+}
+
+const mapStateToProps = (state)=>{
+    return{
+        addedItems: state.addedItems,
+        total: state.total
+    }
+}
+
+const mapDispatchToProps = (dispatch)=>{
+    return{
+        addShipping: ()=>{dispatch(addShipping())},
+        subShipping: ()=>{dispatch(subShipping())}
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Recipe)
